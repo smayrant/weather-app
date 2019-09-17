7d5038e370730a7162de537b0eee84ef52cfe848
Sheldrick <mrmayrant@yahoo.com>
2019-09-17 08:54:12 -0400

    displayed and styled more data related to the current weather

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -25,7 +25,37 @@ class CurrentWeather extends Component {
 					</div>
 					<img src={`https://www.weatherbit.io/static/img/icons/${currentWeather.weather.icon}.png`} alt="" />
 				</div>
-				<div className="current-weather-details" />
+				<div className="current-weather-details">
+					<div className="current-weather-feels-like">
+						<div className="current-weather-feels-like-temp">{Math.round(currentWeather.app_temp)}</div>
+						<p>Feels Like</p>
+					</div>
+					<div className="ui divider" />
+					<div className="current-weather-wind-container">
+						<div className="current-weather-wind-title">Wind</div>
+						<div className="current-weather-wind">{currentWeather.wind_spd} mph</div>
+					</div>
+					<div className="ui divider" />
+					<div className="current-weather-humidity-container">
+						<div className="current-weather-humidity-title">humidity</div>
+						<div className="current-weather-humidity">{currentWeather.rh}%</div>
+					</div>
+					<div className="ui divider" />
+					<div className="current-weather-dewpt-container">
+						<div className="current-weather-dewpt-title">Dew Point</div>
+						<div className="current-weather-dewpt">{currentWeather.dewpt}</div>
+					</div>
+					<div className="ui divider" />
+					<div className="current-weather-pressure-container">
+						<div className="current-weather-pressure-title">Dew Point</div>
+						<div className="current-weather-pressure">{Math.round(currentWeather.pres / 33.864)} in</div>
+					</div>
+					<div className="ui divider" />
+					<div className="current-weather-visibility-container">
+						<div className="current-weather-visibility-title">Visibility</div>
+						<div className="current-weather-visibility">{currentWeather.vis} mi</div>
+					</div>
+				</div>
 			</div>
 		) : (
 			<div>Loading...</div>
